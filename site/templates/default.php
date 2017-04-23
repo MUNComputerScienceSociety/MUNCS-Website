8897cdf7c3d455d0f36124472484708a8b367440
Mark Stacey <markjstacey@gmail.com>
2017-04-23 19:09:50 -0230

    Start over

--- site/templates/default.php
+++
@@ -1,7 +0,0 @@
-<?php snippet('header') ?>
-<div class="content">
-	<?php snippet('breadcrumb') ?>
-	<?= kirbytext($page->text()) ?>
-</div>
-<div style="clear:both"></div>
-<?php snippet('footer') ?>
