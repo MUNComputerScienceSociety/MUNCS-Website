7b68e837216a04b0e6191db3da12ef19195f0868
Jack Arthur Harrhy <me@jackharrhy.com>
2019-10-02 14:15:06 -0230

    add automagical faculty page generator

---
+++ content/resources/faculty/generate-faculty-page.fish
@@ -0,0 +1,39 @@
+#!/usr/bin/env fish
+#
+# this file expects to be ran on labnet itself!
+# it'll output the file you see in this dir, contents.lr, auto-magically :star:
+
+function print_list_of_homepages
+  set old_dir (pwd)
+
+  cd $argv[1]
+
+  for f in */
+    set name (string replace "/" "" $f)
+    set homepage "http://www.cs.mun.ca/~$name/"
+
+    if curl --output /dev/null --silent --head --fail $homepage
+      set common_name (string replace "cn: " "" (ldapsearch -ZZ -LLL "(uid=$name)" | grep "cn: "))
+      printf '* %s - [%s](%s)\n' $common_name $name $homepage
+    end
+  end
+
+  cd $old_dir
+end
+
+function facutly_page
+  echo "title: Faculty"
+  echo "---"
+  echo "body:"
+  echo ""
+  echo "# Faculty"
+  echo ""
+  echo "Below is a _noncomprehensive_ list of homepages for various computer science faculty members, potentially outdated/dead links."
+  echo ""
+
+  print_list_of_homepages "/users/cs/faculty"
+  print_list_of_homepages "/users/cs/staff"
+  print_list_of_homepages "/users/cs/staff2"
+end
+
+facutly_page > contents.lr
