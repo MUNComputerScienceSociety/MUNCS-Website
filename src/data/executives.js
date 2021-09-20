c66a0809b37fe0aa1dc628aa27196086860b5451
Jack Arthur Harrhy <me@jackharrhy.com>
2021-09-20 03:11:06 -0230

    discord name in exec list

--- src/data/executives.js
+++ src/data/executives.js
@@ -1,3 +1,10 @@
+function cleanJacksNameLol(input) {
+    input = input.replace(/&/g, '&amp;');
+    input = input.replace(/</g, '&lt;');
+    input = input.replace(/>/g, '&gt;');
+    return input;
+}
+
 // TODO executive avatars
 // TODO previous executives
 export const EXECUTIVES = [
@@ -5,26 +12,46 @@ export const EXECUTIVES = [
 		name: "Madison Emshey",
 		roles: ["President"],
 		username: "mbemshey",
+		discord: {
+			username: "Modstrosus",
+			discriminator: "0001",
+		},
 	},
 	{
 		name: "Jack Harrhy",
 		roles: ["Vice President"],
 		username: "jaharrhy",
+		discord: {
+			username: cleanJacksNameLol("<i>jack arthur null</i>"),
+			discriminator: "7539",
+		},
 	},
 	{
 		name: "David Chicas",
 		roles: ["Executive Assistant"],
 		username: "dchicasduena",
+		discord: {
+			username: "dech",
+			discriminator: "0089",
+		},
 	},
 	{
 		name: "Rishi Gandhi",
 		roles: ["International Student Representative"],
 		username: "rrgandhi",
+		discord: {
+			username: "R1SH1",
+			discriminator: "6363",
+		},
 	},
 	{
 		name: "Vilakshan Khanna",
 		roles: ["Social Representative"],
 		username: "vkhanna",
+		discord: {
+			username: "Vilakshan",
+			discriminator: "6171",
+		},
 	},
 	{
 		name: "Zac Batten",
@@ -39,6 +66,10 @@ export const EXECUTIVES = [
 		name: "Mathew Emshey",
 		roles: ["Representative-at-Large"],
 		username: "memshey",
+		discord: {
+			username: "maegpi",
+			discriminator: "5707",
+		},
 	},
 	{
 		name: "Abhijeet Singh",
