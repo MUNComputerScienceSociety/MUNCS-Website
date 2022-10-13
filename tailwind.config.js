d022e570165dcda6b614109bce9716dded1c2a31
Jack Arthur Harrhy <me@jackharrhy.com>
2022-10-13 11:30:45 -0230

    update astro to latest
    - rejig astro.config.mjs, changed quite a bit from when we wrote it
    - import react & tailwind as intergrations
    - reformatingfiles here & there (tailwind, astro files, etc.)
    - astro removed the Markdown component, some things are now .md files,
      other things are HTML under the markdown class
    - typescript errors, solved with any ;)
    - images in public folder imported directly, didn't want to break the
      ability to link / reference these static assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,25 @@
 module.exports = {
-	darkMode: "media",
-	mode: "jit",
-	purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}"],
-	theme: {
-		extend: {
-			colors: {
-				background: {
-					DEFAULT: "white",
-					dark: "#18191A",
-				},
-				foreground: {
-					DEFAULT: "black",
-					dark: "#EDEDED",
-				},
-				hover: {
-					DEFAULT: "#F3F4F6",
-					dark: "#2f2f30",
-				},
-			},
-		},
-	},
+  darkMode: "media",
+  content: [
+    "./public/**/*.html",
+    "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: {
+          DEFAULT: "white",
+          dark: "#18191A",
+        },
+        foreground: {
+          DEFAULT: "black",
+          dark: "#EDEDED",
+        },
+        hover: {
+          DEFAULT: "#F3F4F6",
+          dark: "#2f2f30",
+        },
+      },
+    },
+  },
 };
