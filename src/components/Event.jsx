8af0c78ce4ca110b8f67c5ddeac4292d2c040f0b
Jack Arthur Harrhy <me@jackharrhy.com>
2022-10-20 20:46:03 -0230

    fix mobile view of events on the homepage

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,7 +1,7 @@
 import { format } from "date-fns";
 
 export const EventDate = ({ date }) => (
-  <li>
+  <li className="flex flex-col md:flex-row justify-between">
     {date.title}
     <span className="italic float-right opacity-50 dark:opacity-70">
       {format(date.time, "MMMM do, yyyy")}
@@ -10,7 +10,7 @@ export const EventDate = ({ date }) => (
 );
 
 export const EventDates = ({ dates }) => (
-  <ul className="list-disc pl-5">
+  <ul className="list-disc pl-0 md:px-2">
     {dates.map((date) => (
       <EventDate key={date.time} date={date} />
     ))}
@@ -19,8 +19,8 @@ export const EventDates = ({ dates }) => (
 
 const Event = ({ event }) => (
   <a href={event.link}>
-    <div className="border px-3 py-2 rounded-sm mb-2">
-      <h2 className="font-semibold">{event.title}</h2>
+    <div className="border px-3 py-2 rounded-sm mb-2 pb-2.5">
+      <h2 className="font-semibold mb-1">{event.title}</h2>
       <EventDates dates={event.dates} />
     </div>
   </a>
