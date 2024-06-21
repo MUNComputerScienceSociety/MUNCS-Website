ff9e40e26355a317c88dadf0ac530a2256915898
Jack Arthur Harrhy <me@jackharrhy.com>
2024-06-20 21:46:51 -0230

    update all the things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
--- snowpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	mount: {},
-	plugins: [],
-	packageOptions: {},
-	devOptions: {},
-	buildOptions: {},
-};
