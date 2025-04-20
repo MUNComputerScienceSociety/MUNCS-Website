43e97c2622b6e79365b55ecaacf4aa95284090c2
Jack Arthur Harrhy <me@jackharrhy.com>
2025-04-19 21:31:36 -0230

    remove links to fb
    dead app

--- src/data/links.js
+++ src/data/links.js
@@ -4,8 +4,6 @@ export const LINKS = {
   githubExecDocs:
     "https://github.com/MUNComputerScienceSociety/Executive-Docs/",
   csclubExecDocs: "https://www.cs.mun.ca/~csclub/executive-documents/",
-  facebookPage: "https://www.facebook.com/MUNCSSociety/",
-  facebookGroup: "https://www.facebook.com/groups/2202189009/",
   instagram: "https://www.instagram.com/muncssociety/",
   twitter: "https://twitter.com/muncssociety/",
   ctsnl: "https://ctsnl.ca/",
